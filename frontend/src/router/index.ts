f043d64f511e7495ec75152e2d89e0920559d6b5
zy7y <13271962515@163.com>
2022-04-15 23:27:53 +0800

    fix：deps lib

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -5,6 +5,10 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: "/",
+      redirect: "/login",
+    },
+    {
       path: "/login",
       name: "Login",
       component: () => import("@/views/login/Login"),
